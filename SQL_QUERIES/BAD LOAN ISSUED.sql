76908f4e7ec7a6ef912027b96cc770eb4bed616e
Jeevithachaluvaraj <157381804+Jeevithagowda18@users.noreply.github.com>
2026-01-16 20:19:14 +0530

    Update BAD LOAN ISSUED.sql

--- SQL_QUERIES/BAD LOAN ISSUED.sql
+++ SQL_QUERIES/BAD LOAN ISSUED.sql
@@ -11,8 +11,11 @@ WHERE loan_status = 'Charged Off'
 
 
 --Bad Loan Funded Amount
+SELECT SUM(loan_amount) AS Bad_Loan_Funded_amount FROM bank_loan_data
+WHERE loan_status = 'Charged Off'
 
-
+	
 --Bad Loan Amount Received
 SELECT SUM(total_payment) AS Bad_Loan_amount_received FROM bank_loan_data
 WHERE loan_status = 'Charged Off'
+
